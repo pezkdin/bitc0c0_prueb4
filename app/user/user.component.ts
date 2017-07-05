08df409dd28836dbd1b1a9b9f1f658a8361cbf54
pezkdin <pezkdimm@gmail.com>
2017-07-05 01:00:29 -0300

    1ra subida de archivos

---
+++ app/user/user.component.ts
@@ -0,0 +1,36 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AlertService, UserService } from '../_services/index';
+import { User } from '../_models/index';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'user.component.html'
+})
+
+
+export class UserComponent implements OnInit {
+    model: any = {};
+    loading = false;
+    users: User[] = [];
+
+    constructor(
+        private router: Router,
+        private userService: UserService,
+        private alertService: AlertService) { 
+    
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll().subscribe(users => { this.users = users; });
+    }
+}
